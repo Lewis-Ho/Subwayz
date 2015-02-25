3d219412f94d1692e3ab5c440e49ee669123b604
Lewis-Ho <yiucheungho@Yius-MacBook-Pro.local>
2015-02-24 19:10:32 -0500

    google-place-api-testing

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'google_places'
+
 #gem 'ruby-protocol-buffers'
 
 # Use ActiveModel has_secure_password
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
+  require 'google_places'
+  
   def index
+    key = "AIzaSyBEDBGSYZACJSTFx3EBAUpi7Ni90VWA5hM"
+    @key = key
+    @client = GooglePlaces::Client.new(key)
+    
+    @station = @client.spots(40.7851185, -73.72719089999998, :types => 'subway')
+    #puts "AIzaSyBEDBGSYZACJSTFx3EBAUpi7Ni90VWA5hM"
   end
   
   def station
