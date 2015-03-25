5aa8bc9ea104a4cf41dd881f4836386d3b76a467
Luther <lutherling12@gmail.com>
2015-03-25 03:57:17 -0400

    Map needs to be corrected.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -100,13 +100,16 @@ $(document).ready(function(){
   directionsService = new google.maps.DirectionsService();
   // Initial map 
   function initialize() {
+    var pos;
     directionsDisplay = new google.maps.DirectionsRenderer();
     var mapOptions = {
       zoom: 13
     };
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        //var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        
         var marker = new google.maps.Marker({
           position: pos,
           map: map,
@@ -151,6 +154,9 @@ $(document).ready(function(){
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
+
+  google.maps.event.trigger(map, 'resize');
+  maps.setCenter(pos);
 });
 
 // Set route and request direction result 
